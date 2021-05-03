6c88ef3d0caf3173bffb54fc0520c92ea6a94468
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-05-03 12:57:17 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -165,13 +165,13 @@ public class SignUp_Step extends SetupClass {
 	}
 	@Then("^Stripe Checkout Redirection$")
 	public void Stripe_Checkout_Redirection() throws Throwable {
-	 String co_page_title=driver.getTitle();
+	 String stripe_page_title=driver.getTitle();
 		Thread.sleep(3000);
 	    System.out.println("Title of the Page is --> "+stripe_page_title);
 	    
 	    String page_title="https://checkout.stripe.com/";
 	    
-	    if(page_title.equalsIgnoreCase(co_page_title))
+	    if(page_title.equalsIgnoreCase(stripe_page_title))
 	    {
 	    	System.out.println(" user is on the Stripe page");
 	    	log.info("USER IS ON THE STRIPE PAGE");
@@ -185,11 +185,11 @@ public class SignUp_Step extends SetupClass {
 		Thread.sleep(3000);
 		 WebElement Stripe_email = driver.findElement(By.cssSelector("#email"));
 		Thread.sleep(2000);
-		 new_email.sendKeys("slidetech.qa@gmail.com");
+		 Stripe_email.sendKeys("slidetech.qa@gmail.com");
 		Thread.sleep(2000);
 		 WebElement Stripe_back = driver.findElement(By.cssSelector("#root > div > div > div.App-Overview > header > div > div > a > div > div > div.Header-backArrowContainer > svg"));
 		Thread.sleep(2000);
-			Stripe_back.clcik();
+			Stripe_back.click();
 		Thread.sleep(5000);
 		//driver.get("https://www.slidegeeks.com/component/pago/checkout");
 		//Thread.sleep(2000);
