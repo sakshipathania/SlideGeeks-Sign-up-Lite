1816bb37f56b510e2ce11ad0527b5e9d96fd546f
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-10-19 12:59:54 +0530

    Update SignUp_Step.java

--- src/test/java/TestRunner/loginSteps/SignUp_Step.java
+++ src/test/java/TestRunner/loginSteps/SignUp_Step.java
@@ -158,7 +158,9 @@ public class SignUp_Step extends SetupClass {
 		 try {
 		Thread.sleep(1400);
 		// select stripe option
-		WebElement co_btn  =  wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-checkout-billing-payment-form > div > div:nth-child(1) > label")));
+		//WebElement co_btn  =  wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#pg-checkout-billing-payment-form > div > div:nth-child(1) > label")));
+	
+		WebElement  co_btn = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[4]/div[1]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/input[1]"));
 		Thread.sleep(2000);
 	         co_btn.click();
 		Thread.sleep(5000);
